b7a6fd21bf6ca73e2735cd4669b0df84fa7b0f7d
Mona <mdima3501@gmail.com>
2026-01-07 10:43:10 +0300

    Edited version from v0.002 to cls 0.002

--- src/basecont_.go
+++ src/basecont_.go
@@ -1,6 +1,6 @@
 package main
 
-const Version string = "v0.002"
+const Version string = "cls 0.002"
 
 const BaseCppFile string = "#include <iostream>\n\nint main()\n{\n\tstd::cout << " + `"Hello, World!"` + "<< std::endl;\n\treturn 0;\n}\n"
 const BaseTestCppFile string = "\n// In main function here you can write your tests\nint main() {  return 0;  }\n"
