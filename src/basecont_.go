3a9a754b4e3e0e7e7875eba254031a8555e4dd9a
Mona <mdima3501@gmail.com>
2026-01-28 15:59:21 +0300

    replace no needed \n in basecont and added tab for base C code

--- src/basecont_.go
+++ src/basecont_.go
@@ -6,5 +6,5 @@ const BaseCppFile string = "#include \"../headers/include.hpp\"\n\nint main()\n{
 const BaseIncludeHPP string = "#include <iostream>\n#pragma once\n"
 const BaseTestFile string = "\n// In main function here you can write your tests\nint main() {  return 0;  }\n"
 
-const BaseCFile string = "#include \"../headers/include.h\"\n\nint main()\n{\n\tprintf(\"Hello, World!\\n\");\n\nreturn 0;\n}\n"
+const BaseCFile string = "#include \"../headers/include.h\"\n\nint main()\n{\n\tprintf(\"Hello, World!\\n\");\n\treturn 0;\n}\n"
 const BaseHFile string = "#include <stdio.h>\n#ifndef HEADER_H\n#define HEADER_H\n\n\n\n#endif\n"
