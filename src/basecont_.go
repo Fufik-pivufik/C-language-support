11b8ea32a682004bbe29f8e3409007d872b79821
Mona <mdima3501@gmail.com>
2026-03-13 18:50:57 +0300

    replaced HEADER_H in include.h file by INCLUDE_H

--- src/basecont_.go
+++ src/basecont_.go
@@ -8,4 +8,4 @@ const BaseIncludeHPP string = "#include <iostream>\n#pragma once\n"
 const BaseTestFile string = "\n// In main function here you can write your tests\nint main() {  return 0;  }\n"
 
 const BaseCFile string = "#include \"../include/include.h\"\n\nint main()\n{\n\tprintf(\"Hello, World!\\n\");\n\treturn 0;\n}\n"
-const BaseHFile string = "#include <stdio.h>\n#ifndef HEADER_H\n#define HEADER_H\n\n\n\n#endif\n"
+const BaseHFile string = "#include <stdio.h>\n#ifndef INCLUDE_H\n#define INCLUDE_H\n\n\n\n#endif\n"
